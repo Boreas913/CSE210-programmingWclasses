b3792cad4c053e0066cc00cb3bce496f2d9a7f06
Aurora Smith <Auroracsmith913@gmail.com>
2026-01-14 16:36:24 -0700

    prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int RandomNumber = randomGenerator.Next(1, 100);
+
+
+        Console.Write("This is the \"guess a number\" game.");
+        Console.Write("You try to guess a random number in the smallest number of attempts.");
+        Console.Write("");
+        Console.Write("Guess a number between 1 and 100.");
+        int num_guesses = 0;
+        int guess = -1;
+        
+        while (guess != RandomNumber)
+        {  
+            guess = int.Parse(Console.ReadLine());
+            num_guesses += 1;
+            if (guess > RandomNumber){ Console.Write("     Too high!");}
+            else if (guess < RandomNumber){Console.Write("     Too low!");}
+            else{ Console.Write($"You were able to find the number in {num_guesses} guesses.");};
+        };
     }
 }
