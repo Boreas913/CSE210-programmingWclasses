de62901ee4d72ee687ab082e52a9803f9594dfc3
Aurora Smith <Auroracsmith913@gmail.com>
2026-01-14 16:57:05 -0700

    prep 4

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int input = -1;
+        int sum = 0;
+        int largest = 0;
+
+        while (input != 0)
+        {
+            Console.Write("Enter a Number: ");
+            input = int.Parse(Console.ReadLine());
+            numbers.Add(input);
+        }
+        foreach (int number in numbers)
+        {
+            sum += number;
+        }
+        foreach (int number in numbers)
+        {
+            if (number > largest)
+            {
+                largest = number;
+            }
+        }
+        int average = sum / numbers.Count;
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The largest number is: {largest}");
     }
 }
