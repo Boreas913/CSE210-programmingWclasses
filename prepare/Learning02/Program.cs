85c41f5fcd9a75f1c1c7c907c924b3fcda23de48
Aurora Smith <Auroracsmith913@gmail.com>
2026-01-20 19:34:36 -0700

    learning02

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Web dev";
+        job1._startYear = 2027;
+        job1._endYear = 2029;
+
+        job1.Display(); 
+
+        Job job2 = new Job();
+        job2._company = "Apple";
+        job2._jobTitle = "UX internship";
+        job2._startYear = 2027;
+        job2._endYear = 2030;
+
+        job2.Display(); 
+
+        Resume myResume = new Resume();
+        myResume._name = "Jane Doe";
+        myResume. _jobs.Add(job1);
+        myResume. _jobs.Add(job2);
+
+        myResume.Display();
     }
 }
