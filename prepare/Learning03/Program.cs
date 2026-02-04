9951dc867a18e87b466c87a6adf12663adefc665
Aurora Smith <Auroracsmith913@gmail.com>
2026-02-03 18:43:38 -0700

    encapsulation fraction code

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,32 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction fraction1 = new Fraction();
+        Console.WriteLine(fraction1.GetFractionString());
+        Console.WriteLine(fraction1.GetDecimalValue());
+
+        Fraction fraction2 = new Fraction(5);
+        Console.WriteLine(fraction2.GetFractionString());
+        Console.WriteLine(fraction2.GetDecimalValue());
+
+        Fraction fraction3 = new Fraction(3, 4);
+        Console.WriteLine(fraction3.GetFractionString());
+        Console.WriteLine(fraction3.GetDecimalValue());
+
+        Fraction fraction4 = new Fraction(1, 3);
+        Console.WriteLine(fraction4.GetFractionString());
+        Console.WriteLine(fraction4.GetDecimalValue());
+
+        Random random= new Random();
+        Fraction fraction5 = new Fraction();
+        for (int i = 0; i < 20; i++)
+        {
+            int topValue = random.Next(1, 100);
+            int bottomValue = random.Next(1, 100);
+            fraction5.SetTop(topValue);
+            fraction5.SetBottom(bottomValue);
+            Console.Write($"Fraction: {fraction5.GetFractionString()}");
+            Console.WriteLine($"Decimal: {fraction5.GetDecimalValue()}");
+        }
     }
 }
