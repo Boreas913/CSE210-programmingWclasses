42e4a8a4f052872e3766b7b78fd0a5e5a7b4e734
Aurora Smith <Auroracsmith913@gmail.com>
2026-02-18 15:40:46 -0700

    last test

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
+        Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+        Console.WriteLine(assignment.GetSummary());
         WritingAssignment writing = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
         MathAssignment math = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-9");
         Console.WriteLine(writing.GetSummary());
