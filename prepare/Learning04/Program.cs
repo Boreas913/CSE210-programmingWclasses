f119d7e2f3538a411fa093276c448273e3ad51e0
Aurora Smith <Auroracsmith913@gmail.com>
2026-02-18 15:39:07 -0700

    inheritance playground done

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        WritingAssignment writing = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+        MathAssignment math = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-9");
+        Console.WriteLine(writing.GetSummary());
+        Console.WriteLine(writing.GetWritingInformation());
+        Console.WriteLine(" ");
+        Console.WriteLine(math.GetSummary());
+        Console.WriteLine(math.GetHomeworkList());
     }
 }
