503b21d48dcb71bb57121c9d7e7461f5888a3c3b
Aurora Smith <Auroracsmith913@gmail.com>
2026-03-04 16:22:05 -0700

    shapes done

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,12 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> list = new List<Shape>();
+        list.Add(new Square("Red", 5));
+        list.Add(new Rectangle("Blue", 4, 6));
+        list.Add(new Circle("Green", 3));
+        foreach (Shape s in shapes) { 
+            Console.WriteLine($"{s.GetColor()} shape area: {s.GetArea()}"); 
+        }
     }
 }
